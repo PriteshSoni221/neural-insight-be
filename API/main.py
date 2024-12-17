bb75adb9558d62d73219efaf69eb82b4e3d72005
Pritesh Soni <priteshsoni263@gmail.com>
2024-12-17 23:46:48 +0100

    feat: CORS middleware added

--- API/main.py
+++ API/main.py
@@ -2,9 +2,21 @@ from typing import Union
 from transformers import pipeline, AutoModelForSeq2SeqLM, AutoTokenizer
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:4200"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 local_dir = "facebook1/bart-large-cnn"
 model_local = AutoModelForSeq2SeqLM.from_pretrained(local_dir)
@@ -13,7 +25,7 @@ tokenizer_local = AutoTokenizer.from_pretrained(local_dir)
 summarizer = pipeline(task="summarization", model=model_local,  tokenizer=tokenizer_local, device=0)
 
 class SummarizationRequest(BaseModel):
-    text: str
+    review: str
 
 
 @app.get("/")
@@ -29,7 +41,8 @@ async def read_item(item_id: int, q: Union[str, None] = None):
 @app.post("/summarize")
 async def summarize(request: SummarizationRequest):
     try:
-        summary = summarizer(request.text, max_length=150, min_length=40, do_sample=False)
+        summary = summarizer(request.review, max_length=150, min_length=40, do_sample=False)
         return {"summary": summary[0]["summary_text"]}
-    except Exception as e:
+    except HTTPException as e:
+        print("error")
         raise HTTPException(status_code=500, detail=str(e))
