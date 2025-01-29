7d2bc99c8dff3556d26881148fdf2778b2ad9fe6
PriteshSoni221 <priteshsoni263@gmail.com>
2025-01-29 17:47:53 +0100

    dummy response: summarize API

--- API/main.py
+++ API/main.py
@@ -18,11 +18,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-local_dir = "facebook1/bart-large-cnn"
-model_local = AutoModelForSeq2SeqLM.from_pretrained(local_dir)
-tokenizer_local = AutoTokenizer.from_pretrained(local_dir)
+# local_dir = "facebook1/bart-large-cnn"
+# model_local = AutoModelForSeq2SeqLM.from_pretrained(local_dir)
+# tokenizer_local = AutoTokenizer.from_pretrained(local_dir)
 
-summarizer = pipeline(task="summarization", model=model_local,  tokenizer=tokenizer_local, device=0)
+# summarizer = pipeline(task="summarization", model=model_local,  tokenizer=tokenizer_local, device=0)
 
 class SummarizationRequest(BaseModel):
     review: str
@@ -41,8 +41,9 @@ async def read_item(item_id: int, q: Union[str, None] = None):
 @app.post("/summarize")
 async def summarize(request: SummarizationRequest):
     try:
-        summary = summarizer(request.review, max_length=150, min_length=40, do_sample=False)
-        return {"summary": summary[0]["summary_text"]}
+        # summary = summarizer(request.review, max_length=150, min_length=40, do_sample=False)
+        # return {"summary": summary[0]["summary_text"]}
+        return {"summary": "lorem ipsum"}
     except HTTPException as e:
         print("error")
         raise HTTPException(status_code=500, detail=str(e))
---
+++ README.md
@@ -0,0 +1,10 @@
+## Install dependencies
+Go to "API" directory and install dependencies using pip
+```
+pip install -r requirements.txt
+```
+
+## Run the server using fast api command
+```
+fastapi dev main.py
+```
